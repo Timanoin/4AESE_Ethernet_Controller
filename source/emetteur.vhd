d1d6999654ef0e3bca1b61b306d21c6763d3f067
Arthur Gautheron <gauthero@etud.insa-toulouse.fr>
2022-11-09 14:01:19 +0100

    mise au propre, ajour du resetn

--- source/emetteur.vhd
+++ source/emetteur.vhd
@@ -6,58 +6,60 @@ use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 entity emetteur is
-    port (TABORTP, TAVAILP, TFINISHP, TLASTP, CLK, RESET : in std_logic;
+    port (TABORTP, TAVAILP, TFINISHP, TLASTP, CLK, RESETN : in std_logic;
           TDATAI  : in std_logic_vector(7 downto 0);
           TSTARTP, TREADDP, TDONEP, TRNSMTP  : out std_logic;
           TDATAO : out std_logic_vector(7 downto 0));
 end emetteur;
 
 architecture behavioral of emetteur is 
-
-constant SFD : std_logic_vector(7 downto 0) := "10101011";
-constant EFD : std_logic_vector(7 downto 0) := "01010100";
-constant ADDR_SRC : std_logic_vector(47 downto 0) := X"ABABABABABAB";
-
-signal TRNSMTP_s : std_logic := '0'; 
-signal TSTARTP_s : std_logic := '0'; 
-signal TREADDP_s : std_logic := '0'; 
-signal TDONEP_s  : std_logic := '0'; 
-
-signal SFD_done  : std_logic := '0'; 
-signal EFD_done  : std_logic := '0'; 
-signal src_done  : std_logic := '0'; 
-signal dest_done : std_logic := '0';
-signal DATA_done : std_logic := '0';  
-
-signal cmp_src  : std_logic_vector(2 downto 0) := "000"; 
-signal cmp_dest : std_logic_vector(2 downto 0) := "000"; 
-signal cmp_clk : std_logic_vector(7 downto 0) := "00000000"; 
-
-signal CLK8     : std_logic := '0';
-signal RST_CLK8 : std_logic := '1';
-
-
+    -- Vecteurs constants
+    constant SFD : std_logic_vector(7 downto 0) := "10101011";
+    constant EFD : std_logic_vector(7 downto 0) := "01010100";
+    constant ADDR_SRC : std_logic_vector(47 downto 0) := X"ABABABABABAB";
+    -- Signaux de sortie intermédiaires
+    signal TRNSMTP_s : std_logic := '0'; 
+    signal TSTARTP_s : std_logic := '0'; 
+    signal TREADDP_s : std_logic := '0'; 
+    signal TDONEP_s  : std_logic := '0'; 
+    -- Signaux booleens
+    signal SFD_done  : std_logic := '0'; 
+    signal EFD_done  : std_logic := '0'; 
+    signal src_done  : std_logic := '0'; 
+    signal dest_done : std_logic := '0';
+    signal DATA_done : std_logic := '0';  
+    -- Compteurs
+    signal cmp_src  : std_logic_vector(2 downto 0) := "000"; 
+    signal cmp_dest : std_logic_vector(2 downto 0) := "000"; 
+    signal cmp_clk : std_logic_vector(7 downto 0) := "00000000"; 
+    -- Horloges
+    signal CLK8     : std_logic := '0';
+    signal RST_CLK8 : std_logic := '1';
 begin 
+    -- Process synchrone sur la clock de base : 
+    -- gestion des impulsions, 
+    -- observation du début d'émission.
     process (CLK)
     begin
-        if rising_edge(CLK) then
-            if TAVAILP = '1' and TRNSMTP_s = '0' then 
-                -- Début émission            
+        if rising_edge(CLK) and RESETN = '1' then
+            -- Début émission
+            if TAVAILP = '1' and TRNSMTP_s = '0' then             
                 TRNSMTP_s <= '1';
                 TSTARTP_s <= '1';
                 -- Activation Clock 8 bits
                 RST_CLK8 <= '0';
             end if;
-            -- Gestion impulsions
+            -- Gestion des impulsions
             if TREADDP_s ='1' then TREADDP_s <='0'; end if;            
             if TDONEP_s  ='1' then TDONEP_s  <='0'; end if;                
             if TSTARTP_s ='1' then TSTARTP_s <='0'; end if;        
         end if;
     end process;
-
+    
+    -- Process synchrone sur la CLK8 : actif tous les 8 fronts de CLK
     process (CLK8)
     begin
-        if rising_edge(CLK8) then
+        if rising_edge(CLK8) and RESETN = '1' then
             if SFD_done = '0' then
                 TDATAO <= SFD;
                 SFD_done <= '1';
@@ -103,21 +105,22 @@ begin
         end if;    
     end process;
     
+    -- Générateur d'une clock de fréquence fCLK/8 : gestion des octets
+    -- Active lorsque RST_CLK8 est à 0 (choix du moment de fonctionnement)
     CLK8_generator : process(CLK,RST_CLK8)
     begin 
-    if rising_edge(CLK) then 
-        if RST_CLK8 = '1' then
-            CLK8 <= '0';
-            cmp_clk <= "00000100"; 
-        else 
-            CLK8 <= cmp_clk(2);
-            cmp_clk <= cmp_clk + 1;    
+        if rising_edge(CLK) and RESETN = '1' then 
+            if RST_CLK8 = '1' then
+                CLK8 <= '0';
+                cmp_clk <= "00000100"; 
+            else 
+                CLK8 <= cmp_clk(2);
+                cmp_clk <= cmp_clk + 1;    
+            end if;
         end if;
-    end if;
     end process;
-        
-    
-    --ASSIGNATION SORTIES
+           
+    --Assignation des sorties
     TRNSMTP <= TRNSMTP_s; 
     TSTARTP <= TSTARTP_s;
     TREADDP <= TREADDP_s;
--- test/emetteur_test.vhd
+++ test/emetteur_test.vhd
@@ -9,43 +9,56 @@ end emetteur_test;
 
 architecture behavioral of emetteur_test is
 component emetteur
-   port (TABORTP, TAVAILP, TFINISHP, TLASTP, CLK, RESET : in std_logic;
+   port (TABORTP, TAVAILP, TFINISHP, TLASTP, CLK, RESETN : in std_logic;
           TDATAI  : in std_logic_vector(7 downto 0);
           TSTARTP, TREADDP, TDONEP, TRNSMTP  : out std_logic;
           TDATAO : out std_logic_vector(7 downto 0));
 end component; 
-signal TABORTP_s, TAVAILP_s, TFINISHP_s, TLASTP_s, CLK_s, RESET_s : in std_logic :='0';
-signal TDATAI_s  : in std_logic_vector(7 downto 0) :="00000000" ;
-signal TSTARTP_s, TREADDP_s, TDONEP_s, TRNSMTP_s  : out std_logic :='0' ;
-signal TDATAO_s : out std_logic_vector(7 downto 0);
-
+signal CLK_s     : in std_logic := '0';
+signal RESETN_s  : in std_logic := '0';
+-- input signals
+signal TABORTP_s : in std_logic := '0';
+signal TAVAILP_s : in std_logic := '0';
+signal TFINISHP_s: in std_logic := '0';
+signal TLASTP_s  : in std_logic := '0';
+signal TDATAI_s  : in std_logic_vector(7 downto 0) := (others => '0') ;
+-- output signals
+signal TSTARTP_s : out std_logic :='0';
+signal TREADDP_s : out std_logic :='0';
+signal TDONEP_s  : out std_logic :='0';
+signal TRNSMTP_s : out std_logic :='0';
+signal TDATAO_s  : out std_logic_vector(7 downto 0);
+-- Clock period
 constant t : time := 10 ns;
 begin
-    
-    Clock_process : process
-    begin 
-    CLK_s <= not(CLK_s);
-    wait for t/2;
-    end process;
-     
     uut : emetteur port map(
         CLK => CLK_s,
-        TABORTP => TABORTP_s,
+        RESETN => RESETN_s,
+        TABORTP => TABORTP_s, 
         TAVAILP => TAVAILP_s,
         TFINISHP => TFINISHP_s,
         TLASTP => TLASTP_s,
-        RESET => RESET_s,
         TDATAI => TDATAI_s,
-        TSTARTP => TSTARTP_s,
+        TSTARTP => TSTARTP_s, 
         TREADDP=>TREADDP_s,
         TDONEP => TDONEP_s,
         TRNSMTP => TRNSMTP_s,
         TDATAO => TADATO_s;    
     );
-    
-    stim_proc : process
+
+    -- Création d'une clock de période t
+    clk_proc : process
     begin 
-    TAVAILP_s <= '0', '1' after 10 ns, '0' after 20 ns;
-    TLASTP_s <= '0', '1' after 1 ns;
+        CLK_s <= not(CLK_s);
+        wait for t/2;
+    end process;
+    
+    -- Signaux de test
+    stimulus_proc : process
+    begin 
+        RESETN <= '0', '1' after 100 ns;
+        TAVAILP_s <= '0', '1' after 110 ns, '0' after 120 ns;
+        TLASTP_s <= '0', '1' after 110 ns;
+    end process;
     
 end;
