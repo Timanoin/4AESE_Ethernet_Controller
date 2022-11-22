d8fba24b3e95c865523c11d54d677a4d5097f3d4
Lagrost Olivier <lagrost@insa-10230>
2022-11-22 10:05:30 +0100

    fin fichier émetteur

--- source/recepteur.vhd
+++ source/recepteur.vhd
@@ -33,11 +33,10 @@ architecture behavioral of recepteur is
     signal cmp_clk   : std_logic_vector(7 downto 0); 
     signal clk_state : std_logic;
     -- Signaux booleens
-    signal SFD_done  : bit; 
-    signal EFD_done  : bit; 
+    signal SFD_done  : bit;  
     signal src_done  : bit; 
     signal dest_done : bit;
-    signal DATA_done : bit;    
+    signal recep_done : bit;    
     -- Vecteurs constants
     constant SFD      : std_logic_vector(7 downto 0)  := "10101011";
     constant EFD      : std_logic_vector(7 downto 0)  := "01010100";
@@ -53,18 +52,18 @@ begin
         if RESETN = '0' then
             RBYTEP_s  <= '0'; 
             RCLEANP_s <= '0'; 
-            RCNVGP_s  <= '0'; 
             RDONEP_s  <= '0';
             RSTARTP_s <= '0';
+            RCNVGP_s  <= '0';        
+            RSMATIP_s <= '0';
             cmp_dest  <= '0';
             cmp_src   <= '0';
             cmp_clk   <= "00000011";
             clk_state <= '0';
             SFD_done  <= '0'; 
-            EFD_done  <= '0'; 
             src_done  <= '0'; 
             dest_done <= '0';
-            DATA_done <= '0';
+            recep_done <= '0';
         elsif rising_edge(CLK) and RENABP = '1' then
             -- Gestion des impulsions
             RBYTEP_s  <= '0';           
@@ -81,49 +80,75 @@ begin
                     end if;
                 elsif RCVNGP_s = '1' then
                     if dest_done = '0' then
-
-                    
-                    -- Envoi du Start Frame Delimitor
-                    if SFD_done = '0' then
-                        TDATAO <= SFD;
-                        SFD_done <= '1';
-                    -- Envoi de l'adresse du destinataire
-                    elsif dest_done ='0' then
-                        TDATAO <= TDATAI;
-                        TREADDP_s <= '1';
-                        cmp_dest <= cmp_dest + 1;
-                        if cmp_dest = 5 then
-                            dest_done <= '1'; 
-                            cmp_dest <= 0;  
-                        end if;
+                        if cmp_dest = '0' then
+                            if RDATAI /= ADDR_DEST(47 downto 40) then
+                                RCLEANP_s <= '1';
+                                RDONEP_s <= '1';
+                                RCVNGP_s <= '0';
+                            end if;
+                        elsif cmp_dest = '1' then
+                            if RDATAI /= ADDR_DEST(39 downto 32) then
+                                RCLEANP_s <= '1';
+                                RDONEP_s <= '1';
+                                RCVNGP_s <= '0';
+                            end if;
+                        elsif cmp_dest = '2' then
+                            if RDATAI /= ADDR_DEST(31 downto 24) then
+                                RCLEANP_s <= '1';
+                                RDONEP_s <= '1';
+                                RCVNGP_s <= '0';
+                            end if;
+                        elsif cmp_dest = '3' then
+                            if RDATAI /= ADDR_DEST(23 downto 16) then
+                                RCLEANP_s <= '1';
+                                RDONEP_s <= '1';
+                                RCVNGP_s <= '0';
+                            end if;  
+                        elsif cmp_dest = '4' then
+                            if RDATAI /= ADDR_DEST(15 downto 8) then
+                                RCLEANP_s <= '1';
+                                RDONEP_s <= '1';
+                                RCVNGP_s <= '0';
+                            end if;
+                        elsif cmp_dest = '5' then
+                            if RDATAI /= ADDR_DEST(7 downto 0) then
+                                RCLEANP_s <= '1';
+                                RDONEP_s <= '1';
+                                RCVNGP_s <= '0';
+                            else 
+                                dest_done <= '1';
+                                RSMATIP_s <= '1';
+                            end if;    
+                        end if;        
+                        cmp_dest <= cmp_dest + 1;     
+                    end if;   
                     -- Envoi l'adresse de la source
                     elsif src_done ='0' then
-                        case cmp_src is 
-                            when 0 =>      TDATAO <= ADDR_SRC(47 downto 40);
-                            when 1 =>      TDATAO <= ADDR_SRC(39 downto 32);
-                            when 2 =>      TDATAO <= ADDR_SRC(31 downto 24);
-                            when 3 =>      TDATAO <= ADDR_SRC(23 downto 16);
-                            when 4 =>      TDATAO <= ADDR_SRC(15 downto 8);
-                            when 5 =>      TDATAO <= ADDR_SRC(7 downto 0);
-                            when others => TDATAO <= (others => '0');
-                        end case;
+                        RDATAO <= RDATAI;                        
                         cmp_src <= cmp_src + 1;
+                        RBYTEP_s <= '1';
                         if cmp_src = 5 then
                             src_done <= '1';
                             cmp_src <= 0;
                         end if;
                     -- Envoi des données
-                    elsif DATA_done = '0' then
-                        TDATAO <= TDATAI;
-                        TREADDP_s <= '1';
-                        if TLASTP ='1' then
-                            DATA_done <= '1';
+                    elsif recep_done = '0' then
+                        RDATAO <= RDATAI;
+                        RBYTEP_s <= '1';
+                        if RDATAI = EFD then
+                            recep_done <= '1';
+                            RCVNGP_s <= '0';
+                            RSMATIP_s <= '0';
+                            RDONEP_s <= '1';
+                            cmp_dest  <= '0';
+                            cmp_src   <= '0';
+                            cmp_clk   <= "00000011";
+                            clk_state <= '0';
+                            SFD_done  <= '0'; 
+                            src_done  <= '0'; 
+                            dest_done <= '0';
+                            recep_done <= '0';
                         end if;
-                    -- Envoi du End Frame Delimitor    
-                    else 
-                        TDATAO <= EFD;
-                        TDONEP_s <= '1';
-                        TRNSMTP_s <= '0';
                     end if;
                 end if; 
             end if;  
@@ -135,8 +160,10 @@ begin
  
    
     --Assignation des sorties
-    TRNSMTP <= TRNSMTP_s; 
-    TSTARTP <= TSTARTP_s;
-    TREADDP <= TREADDP_s;
-    TDONEP  <= TDONEP_s;
+    RCVNGP  <= RCVNGP_s; 
+    RDONEP  <= RDONEP_s;
+    RBYTEP  <= RBYTEP_s;
+    RCLEANP <= RCLEANP_s;
+    RSMATIP <= RSMATIP_s;
+    RSTARTP <= RSTARTP_s;
 end behavioral;  
